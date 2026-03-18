61c842b2febbaa15e09cc2e507f0282decab7940
Animesh Singh <animeishhh@gmail.com>
2026-03-18 11:29:14 +0530

    Day 2: Move Zeroes and Maximum Subarray

---
+++ Array/MaximumSubarray.java
@@ -0,0 +1,38 @@
+package Array;
+
+public class MaximumSubarray {
+
+    // Function to find maximum subarray sum
+    public static int maxSubArray(int[] nums) {
+        int max_so_far = nums[0];
+        int curr_max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            curr_max = Math.max(nums[i], nums[i] + curr_max);
+            max_so_far = Math.max(curr_max, max_so_far);
+        }
+
+        return max_so_far;
+    }
+
+    // Helper function to print array
+    public static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.print("Array: ");
+        printArray(nums);
+
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
---
+++ Array/MoveZeroes.java
@@ -0,0 +1,43 @@
+package Array;
+public class MoveZeroes {
+
+    // Function to move zeroes to end
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+
+        // Move non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zeroes
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    // Helper function to print array
+    public static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method (entry point)
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        System.out.print("Original Array: ");
+        printArray(nums);
+
+        moveZeroes(nums);
+
+        System.out.print("After Moving Zeroes: ");
+        printArray(nums);
+    }
+}
